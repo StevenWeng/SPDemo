2a22ce40d049c5ff35df77a752903fb1de23a664
StevenWeng <stevenweng@systex.com.tw>
2015-03-09 10:58:24 +0800

    init

---
+++ src/main/java/com/systex/demo/SPDemo/App.java
@@ -0,0 +1,13 @@
+package com.systex.demo.SPDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
